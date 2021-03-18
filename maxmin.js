99c8f3f92c47b90d90133c248dd5e986dc641a22
ankita13-1997 <parhiankita@gmail.com>
2021-03-18 17:09:35 +0530

    max and min among random number is generated

---
+++ maxmin.js
@@ -0,0 +1,67 @@
+let a = Math.floor(Math.random() *999);
+console.log(a)
+
+let b = Math.floor(Math.random() *999);
+console.log(b)
+let c = Math.floor(Math.random() *999);
+console.log(c)
+let d = Math.floor(Math.random() *999);
+console.log(d)
+let e = Math.floor(Math.random() *999);
+console.log(e)
+
+
+if(a >b && a>c && a>d && a>e)
+ {
+     console.log("a is max among five");
+ }
+
+ if(b >a && b>c && b>d && b>e)
+ {
+     console.log("b is max among five");
+ }
+
+ if(c >a && c>b && c>d && c>e)
+ {
+     console.log("c is max among five");
+ }
+
+ if(d >a && d>b && d>c && d>e)
+ {
+     console.log("d is max among five");
+ }
+
+ if(e>a && e>b && e>c && e>d) 
+{
+    console.log("e is max among five");
+}
+
+
+
+
+
+
+if(a <b && a,c && a<d && a<e)
+ {
+     console.log("a is min among five");
+ }
+
+ if(b <a && b<c && b<d && b<e)
+ {
+     console.log("b is min among five");
+ }
+
+ if(c <a && c<b && c<d && c<e)
+ {
+     console.log("c is min among five");
+ }
+
+ if(d <a && d<b && d<c && d<e)
+ {
+     console.log("d is min among five");
+ }
+
+ if(e<a && e<b && e<c && e<d) 
+{
+    console.log("e is min among five");
+}
